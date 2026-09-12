41b943bfbc4d285766864a7113a22486b073bd52
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-09-12 22:20:33 +0530

    css and ui

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,45 +1,80 @@
 import { useEffect, useState } from "react";
+import "./pokemon.css";
+import { PokemonCards } from "./PokemonCards";
 
-export const Pokemon = ()=> {
-    const [pokemon, setPokemon] = useState([]);
-    const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
+export const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
 
-    const pokemonFetch = async()=> {
-        try {
-            const res = await fetch(API);
-            const data = await res.json();
-            setPokemon(data);
-            console.log(data);
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=200";
 
-            const pokemonData = data.results.map((currPokemon)=> {
-                //console.log(currPokemon.url);
-                
-            })
-            
-        } catch (error) {
-            console.log(error);
-            
-        };
-    };
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+      //   console.log(data);
 
-    useEffect(()=> {
-        pokemonFetch();
-    },[])
+      const detailedPokemonData = data.results.map(async (curPokemon) => {
+        const res = await fetch(curPokemon.url);
+        const data = await res.json();
+        return data;
+      });
+      //console.log(detailedPokemonData);
+
+      const responseDetails = await Promise.all(detailedPokemonData);
+      console.log(responseDetails);
+      setPokemon(responseDetails);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  //search functionality
+
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (loading) {
     return(
-        <>
-        <section>
-            <header>
-                <h1> Lets Catch Pokémon </h1>
-                <div>
-                    <ul>
-                        {pokemon.map((currPokemon)=> {
-                            console.log(currPokemon);
-                            
-                        })}
-                    </ul>
-                </div>
-            </header>
-        </section>
-        </>
+        <div>
+            <h1>Loading.....</h1>
+        </div>
     )
-}
+  }
+
+  if (error) {
+    return(
+        <div>
+            <h1>{error.message}</h1>
+        </div>
+    )
+  }
+
+  return (
+    <>
+      <section className="container">
+        <header>
+          <h1> Lets Catch Pokémon</h1>
+        </header>
+        <div>
+            <ul className="cards">
+                {pokemon.map((curPokemon)=> {
+                    return <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
+                })
+                }
+            </ul>
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/PokemonCards.jsx
@@ -0,0 +1,14 @@
+
+export const PokemonCards = ({pokemonData})=> {
+    return(
+        <li className="pokemon-cards">
+            <figure>
+        <img
+          src={pokemonData.sprites.other.dream_world.front_default}
+          alt={pokemonData.name}
+          className="pokemon-image"
+        />
+      </figure>
+        </li>
+    );
+};
