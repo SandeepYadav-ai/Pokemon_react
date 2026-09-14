81c9d1f7517ac43517b8c11495dfcda7d9266285
SandeepYadav-ai <yadavsandeepkumar855@gmail.com>
2026-09-14 18:25:46 +0530

    fetching data

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -8,7 +8,7 @@ export const Pokemon = () => {
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
 
-  const API = "https://pokeapi.co/api/v2/pokemon?limit=1351";
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=300";
 
   const fetchPokemon = async () => {
     try {
